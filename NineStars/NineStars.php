69d9887a3e77f0deb5f7ea334d489424fbc39c63
zhangjingxian <osenyursa@gmail.com>
2018-01-31 00:01:40 +0800

    init

---
+++ NineStars/NineStars.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\themes\NineStars;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class NineStars extends AssetBundle
+{
+    public $sourcePath = '@app/themes/NineStars/assets';
+
+    public $css = [
+        'bootstrap/css/bootstrap.min.css',
+        'font-awesome/css/font-awesome.min.css',
+        'css/nivo-lightbox.css',
+        'css/nivo-lightbox-theme/default/default.css',
+        'css/animate.css',
+        'css/style.css',
+        'css/default.css',
+    ];
+    public $js = [
+        'js/jquery.min.js',
+        'js/jquery.easing.min.js',
+        'js/classie.js',
+        'js/gnmenu.js',
+        'js/jquery.scrollTo.js',
+        'js/nivo-lightbox.min.js',
+        'js/stellar.js',
+        'js/custom.js',
+    ];
+}
---
+++ NineStars/site/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jingx
+ * Date: 2018/1/12/0012
+ * Time: 17:35
+ */
+
+?>
+lsdjfkasjdflkasjdfkl
